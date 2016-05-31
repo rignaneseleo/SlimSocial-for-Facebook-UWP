d85ce58eeab45404ede817b4cb742ecc6a48149f
rignaneseleo <rignanese.leo@outlook.com>
2016-05-31 11:27:24 +0200

    v1.2.3

---
+++ SlimSocial for Facebook/App.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace SlimSocial_for_Facebook
+{
+    sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        // Handle shared link
+        protected override async void OnShareTargetActivated(ShareTargetActivatedEventArgs args)
+        {
+            var data = args.ShareOperation.Data;
+
+            if (data.Contains(StandardDataFormats.Text))
+            {
+                var rootFrame = new Frame();
+                string urlShared = await data.GetTextAsync();
+                if (!urlShared.StartsWith("http://") || !urlShared.StartsWith("https://"))
+                {
+                    // if it's not, let's see if it includes an URL in it (prefixed with a message)
+                    int startUrlIndex = urlShared.IndexOf("http:");
+                    if (startUrlIndex > 0)
+                    {
+                        // seems like it's prefixed with a message, let's trim the start and get the URL only
+                        urlShared = urlShared.Substring(startUrlIndex);
+                    }
+
+                    // final step, set the proper Sharer...
+                    var urlSharer = "https://m.facebook.com/sharer.php?u=" + urlShared;
+                    // ... and parse it just in case
+                    urlSharer = Uri.UnescapeDataString(urlSharer);
+
+                    ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+                    localSettings.Values.Add("urlShared", urlSharer); // add it
+                    rootFrame.Navigate(typeof(MainPage), args.ShareOperation);
+                    Window.Current.Content = rootFrame;
+                    Window.Current.Activate();
+                }
+            }
+        }
+        
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Non ripetere l'inizializzazione dell'applicazione se la finestra già dispone di contenuto,
+            // assicurarsi solo che la finestra sia attiva
+            if (rootFrame == null)
+            {
+                // Creare un frame che agisca da contesto di navigazione e passare alla prima pagina
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: caricare lo stato dall'applicazione sospesa in precedenza
+                }
+
+                // Posizionare il frame nella finestra corrente
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // Quando lo stack di esplorazione non viene ripristinato, passare alla prima pagina
+                    // e configurare la nuova pagina passando le informazioni richieste come parametro
+                    // parametro
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Assicurarsi che la finestra corrente sia attiva
+                Window.Current.Activate();
+            }
+        }
+
+        /// <summary>
+        /// Chiamato quando la navigazione a una determinata pagina ha esito negativo
+        /// </summary>
+        /// <param name="sender">Frame la cui navigazione non è riuscita</param>
+        /// <param name="e">Dettagli sull'errore di navigazione.</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Richiamato quando l'esecuzione dell'applicazione viene sospesa. Lo stato dell'applicazione viene salvato
+        /// senza che sia noto se l'applicazione verrà terminata o ripresa con il contenuto
+        /// della memoria ancora integro.
+        /// </summary>
+        /// <param name="sender">Origine della richiesta di sospensione.</param>
+        /// <param name="e">Dettagli relativi alla richiesta di sospensione.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            // TODO: salvare lo stato dell'applicazione e arrestare eventuali attività eseguite in background
+            deferral.Complete();
+        }
+    }
+}
---
+++ SlimSocial for Facebook/SettingPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace SlimSocial_for_Facebook
+{
+    public sealed partial class SettingPage
+    {
+        ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+
+        public SettingPage()
+        {
+            InitializeComponent();
+            LoadSavedValue();
+
+            PackageVersion number = Package.Current.Id.Version; // Get app version
+            version.Text += string.Format(" {0}.{1}.{2}\r\n", number.Major, number.Minor, number.Build);
+
+            SystemNavigationManager.GetForCurrentView().BackRequested += (s, a) => // add the back event
+            {
+                if (Frame.CanGoBack)
+                {
+                    Frame.GoBack();
+                    a.Handled = true; // This block the app closing
+                }
+            };
+        }
+
+        private void LoadSavedValue()
+        {
+            if (localSettings.Values.ContainsKey("blockTopBar"))
+                blockTopBar.IsOn = (bool)localSettings.Values["blockTopBar"];
+            if (localSettings.Values.ContainsKey("showRecentNews"))
+                showRecentNews.IsOn = (bool)localSettings.Values["showRecentNews"];
+            if (localSettings.Values.ContainsKey("centerTextPosts"))
+                centerTextPosts.IsOn = (bool)localSettings.Values["centerTextPosts"];
+            if (localSettings.Values.ContainsKey("addSpaceBetweenPosts"))
+                addSpaceBetweenPosts.IsOn = (bool)localSettings.Values["addSpaceBetweenPosts"];
+            if (localSettings.Values.ContainsKey("darkTheme"))
+                darkTheme.IsOn = (bool)localSettings.Values["darkTheme"];
+        }
+
+        private void SaveStatus_Toggled(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ToggleSwitch thisToggle = ((ToggleSwitch)sender);
+            bool isOn = thisToggle.IsOn;
+            string toggleName = thisToggle.Name;
+
+            if (!localSettings.Values.ContainsKey(toggleName))
+                localSettings.Values.Add(toggleName, isOn); // add it
+            else
+                localSettings.Values[toggleName] = isOn; // edit it
+        }
+
+        private async void MichiBra_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var urlMichi = new Uri("https://www.linkedin.com/in/branchesimichele");
+            await Windows.System.Launcher.LaunchUriAsync(urlMichi);
+        }
+
+        private async void LeoRigna_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var urlLeo = new Uri("https://github.com/rignaneseleo");
+            await Windows.System.Launcher.LaunchUriAsync(urlLeo);
+        }
+    }
+}
